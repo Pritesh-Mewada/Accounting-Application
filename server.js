3063b3fb53a4fc55259c221fea64560a72ed8894
Pritesh Mewada <mewadapritesh5@gmail.com>
2017-06-15 11:43:22 +0530

    Openshift edit

--- server.js
+++ server.js
@@ -105,6 +105,8 @@ app.get('/test',function(req,res){
   res.send("hello");
 });
 
-app.listen(process.env.PORT ||'3333',()=>{
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.listen(server_port,server_ip_address,()=>{
   console.log("hello i am started");
 })
